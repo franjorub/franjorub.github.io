aa7cfc6c3548c79965945e1fee59fa6a64ed501e
Francisco Rubial <franjorub@gmail.com>
2018-06-01 18:04:25 -0400

    creadas scenas con scrollmagic

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.title-animation').css('animation-play-state', 'running')
+
+    /**este es mi controlador */
+    let controller = new ScrollMagic.Controller()
+
+    let perfilScene = new ScrollMagic.Scene({
+            triggerElement: '#perfil'
+        })
+        .setClassToggle('#perfil', 'fade-in')
+        .addTo(controller)
+
+    let reactScene = new ScrollMagic.Scene({
+            triggerElement: '#habilidades'
+        })
+        .setClassToggle('#react', 'translate3D')
+        .addTo(controller)
+
+    let javascriptsScene = new ScrollMagic.Scene({
+            triggerElement: '#habilidades'
+        })
+        .setClassToggle('#javascript', 'translate3D')
+        .addTo(controller)
+    let nodesScene = new ScrollMagic.Scene({
+            triggerElement: '#habilidades'
+        })
+        .setClassToggle('#nodejs', 'translate3D')
+        .addTo(controller)
+
+    let portafolioScene = new ScrollMagic.Scene({
+            triggerElement: '#portafoliotrigger'
+        })
+        .setClassToggle('#portafolio', 'scale')
+        .addTo(controller)
+
+
+})
